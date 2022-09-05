e15fe15956bd853751d28603667d3fd7604fa82f
emboni <93706027+emboni@users.noreply.github.com>
2022-09-05 14:11:22 +0200

    opgave 2 push

---
+++ ClassRoomNet60/ClassRoom.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    internal class ClassRoom
+    {
+        private string ClassName;
+
+        public string MyProperty
+        {
+            get { return ClassName; }
+            set { ClassName = value; }
+        }
+
+        public List<Student>
+
+
+        datetime DateTime semesterStart;
+
+        public ClassRoom()
+        {
+
+        }
+    }
+}

63d31e6ebbcf745c8e0eabc08526c446ee2244c1
emboni <93706027+emboni@users.noreply.github.com>
2022-09-05 13:26:42 +0200

    5. sep New Coomit

---
+++ ClassRoomNet60/Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    internal class Student
+    {
+		private string name;
+
+		public string MyProperty
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+		private int birthmonth;
+
+		public int MyBirthProperty
+		{
+			get { return birthmonth; }
+			set { birthmonth = value; }
+		}
+
+		private int birthday;
+
+		public int MyBirthdayProperty
+		{
+			get { return birthday; }
+			set { birthday = value; }
+		}
+
+		public Student(string name, string myProperty, int birthmonth, int myBirthProperty, int birthday, int myBirthdayProperty)
+		{
+			this.name = name;
+			MyProperty = myProperty;
+			this.birthmonth = birthmonth;
+			MyBirthProperty = myBirthProperty;
+			this.birthday = birthday;
+			MyBirthdayProperty = myBirthdayProperty;
+		}
+	}
+}
